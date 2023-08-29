c06bd99fcf9cbbaa7df81f5edebb21ccdd748f06
diucap <diucapuig@gmail.com>
2023-08-29 14:11:16 +0200

    token last 20 min

--- trivia-front/back/config.py
+++ trivia-front/back/config.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 import os
 
 from dotenv import load_dotenv
@@ -14,3 +15,5 @@ FLASK_DEBUG = os.environ.get("FLASK_DEBUG", False)
 SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
 JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
 JWT_TOKEN_LOCATION = ["headers"]
+JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes = 20)
+
